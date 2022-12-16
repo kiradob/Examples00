3fe384719ed1f8529bd42e12ce44a01fa5bde95a
kira.dob <arik.dobrynina@yandex.ru>
2022-12-16 13:17:18 +0700

    added Example008

--- Example008_IntroMethod/Program.cs
+++ Example008_IntroMethod/Program.cs
@@ -5,7 +5,7 @@
     if(arg3>result) result=arg3;
     return result;
 }
-// кусочек кода, который отвечает за максимум из трех
+
 int a1=909815;
 int b1=21;
 int c1=39;
@@ -16,13 +16,7 @@ int a3=13;
 int b3=23111;
 int c3=33;
 
-// int max1=Max(a1,b1,c1);
-// int max2=Max(a2,b2,c2);
-// int max3=Max(a3,b3,c3);
-
-// int max=Max(max1,max2,max3);
-
-int max=Max(Max(a1,b1,c1),Max(a2,b2,c2),Max(a3,b3,c3));
+// кусочек кода, который отвечает за максимум из трех
 // if(b1>max) max=b1;
 // if(c1>max) max=c1;
 
@@ -33,6 +27,16 @@ int max=Max(Max(a1,b1,c1),Max(a2,b2,c2),Max(a3,b3,c3));
 // if(a3>max) max=a3;
 // if(b3>max) max=b3;
 // if(c3>max) max=c3;
+
+//Кусочек кода, который делает игру "Поле чудес"
+// int max1=Max(a1,b1,c1);
+// int max2=Max(a2,b2,c2);
+// int max3=Max(a3,b3,c3);
+
+// int max=Max(max1,max2,max3);
+
+int max=Max(Max(a1,b1,c1),Max(a2,b2,c2),Max(a3,b3,c3));
+
 
 Console.Write("max=");
 Console.WriteLine(max);
